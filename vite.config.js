22bea5061091501a1f2960e423e021491095729e
Rishaank Gupta <site.rishaank@gmail.com>
2026-06-15 11:23:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(import.meta.dirname, 'index.html'),
+        compare: resolve(import.meta.dirname, 'compare.html'),
+        developers: resolve(import.meta.dirname, 'developers.html'),
+        about: resolve(import.meta.dirname, 'about.html'),
+      },
+    },
+  },
 });
